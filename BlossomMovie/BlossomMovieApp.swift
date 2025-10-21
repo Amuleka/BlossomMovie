aa75194f3db5d41d70cc1cb8846d06760841473a
amulekangulo <amulek0297@outlook.com>
2025-10-20 19:42:47 -0600

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Amulek Angulo on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlossomMovieTests/BlossomMovieTests.swift
@@ -0,0 +1,16 @@
+//
+//  BlossomMovieTests.swift
+//  BlossomMovieTests
+//
+//  Created by Amulek Angulo on 10/20/25.
+//
+
+import Testing
+
+struct BlossomMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
