b0cdcb9b128776a818890a29abfaa902a96bd0fd
amulekangulo <amulek0297@outlook.com>
2025-10-21 14:15:47 -0600

    Added the rest of the media types to the HomeScreen and modified the getTitles function to use async let for parallel execution

--- BlossomMovie/ViewModel.swift
+++ BlossomMovie/ViewModel.swift
@@ -19,12 +19,24 @@ class ViewModel {
     private(set) var homeStatus: FetchStatus = .notStarted
     private let dataFetcher = DataFetcher()
     var trendingMovies: [Title] = []
+    var trendingTV: [Title] = []
+    var topRatedMovies: [Title] = []
+    var topRatedTV: [Title] = []
     
     func getTitles() async {
         homeStatus = .fetching
         
         do {
-            trendingMovies = try await dataFetcher.fetchTitle(for: "movie", by: "trending")
+            async let tMovies = dataFetcher.fetchTitle(for: "movie", by: "trending")
+            async let tTV = dataFetcher.fetchTitle(for: "tv", by: "trending")
+            async let tRMovies = dataFetcher.fetchTitle(for: "movie", by: "top_rated")
+            async let tRTV = dataFetcher.fetchTitle(for: "tv", by: "top_rated")
+            
+            trendingMovies = try await tMovies
+            trendingTV = try await tTV
+            topRatedMovies = try await tRMovies
+            topRatedTV = try await tRTV
+            
             homeStatus = .success
         } catch {
             print(error)
